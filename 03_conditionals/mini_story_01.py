4c286b65d6ece023af00ee7031535ba854ddf06e
er-omsonar <er.omsonar@gmail.com>
2025-12-08 19:23:45 +0530

    Learning about conditionals in python

---
+++ 03_conditionals/mini_story_01.py
@@ -0,0 +1,6 @@
+is_kettel_boiled = False
+
+if is_kettel_boiled:
+  print("make some tea")
+else:
+  print("buy some tea")
---
+++ 03_conditionals/snack_suggestion.py
@@ -0,0 +1,6 @@
+snack = input("Enter your snack : ").lower()
+if snack == "samosa" or snack == "cookies":
+  print(f"Great Choice! We'll serve you {snack}")
+
+else:
+  print("Sorry, your snack is unavailble.")
